faf1d0b219bbe3e38980280fe0b0bd4a8169fa74
DanielaBadea <badeadaniella@gmail.com>
2024-04-21 02:09:15 +0300

    task 1

---
+++ src/components/Messaje/Message.jsx
@@ -0,0 +1,11 @@
+import styles from './Message.module.css';
+import PropTypes from 'prop-types';
+
+function  Notification ({message}){
+    return  <p className={styles.message}>{message}</p>
+}
+Notification.propTypes = {
+    message: PropTypes.string.isRequired
+}
+
+export default Notification;
